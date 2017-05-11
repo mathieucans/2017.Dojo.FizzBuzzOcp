b8befd2a1397957d0cedc9b4d1dacb4a37ae79f4
CANS Mathieu <MC05359S@atlas.edf.fr>
2017-05-11 12:39:57 +0200

    init

---
+++ FizzBuzzOcp/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzOcp
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void say_1_when_1()
+		{
+			var say = new Say();
+			Assert.AreEqual("1", say.say(1));
+		}
+
+		[TestMethod]
+		public void say_2_when_2()
+		{
+			var say = new Say();
+			Assert.AreEqual("2", say.say(2));
+		}
+	}
+
+	public class Say
+	{
+		public string say(int i)
+		{
+			return i.ToString();
+		}
+	}
+}

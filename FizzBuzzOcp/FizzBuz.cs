d95a367050a1d52f5b16f7ebd14fd07e86f44271
CANS Mathieu <MC05359S@atlas.edf.fr>
2017-05-11 12:51:55 +0200

    refactor step2 done

---
+++ FizzBuzzOcp/FizzBuz.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FizzBuzzOcp
+{
+
+	public interface  IFizzBuzzRule
+	{
+		bool match(int i);
+		string convert(int i);
+	}
+
+	public class EchoRule : IFizzBuzzRule	
+	{
+		public bool match(int i)
+		{
+			return true;
+		}
+
+		public string convert(int i)
+		{
+			return i.ToString();
+		}
+	}
+	
+	public class FizzBuz
+	{
+		private List<IFizzBuzzRule> _list;
+
+
+		public FizzBuz()
+		{
+			_list = new List<IFizzBuzzRule>();
+			_list.Add(new EchoRule());
+		}
+
+		public string say(int i)
+		{
+			return _list.Where(r => r.match(i)).First().convert(i);
+		}
+	}
+}
--- FizzBuzzOcp/FizzBuzzShould.cs
+++ FizzBuzzOcp/FizzBuzzShould.cs
@@ -4,28 +4,27 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FizzBuzzOcp
 {
 	[TestClass]
-	public class UnitTest1
+	public class FizzBuzzShould
 	{
 		[TestMethod]
 		public void say_1_when_1()
 		{
-			var say = new Say();
+			var say = new FizzBuz();
 			Assert.AreEqual("1", say.say(1));
 		}
 
 		[TestMethod]
 		public void say_2_when_2()
 		{
-			var say = new Say();
+			var say = new FizzBuz();
 			Assert.AreEqual("2", say.say(2));
 		}
-	}
 
-	public class Say
-	{
-		public string say(int i)
+	/*	[TestMethod]
+		public void say_Fizz_when_modulo_3()
 		{
-			return i.ToString();
-		}
+			var say = new FizzBuz();
+			Assert.AreEqual("Fizz", say.say(3));
+		}*/
 	}
 }

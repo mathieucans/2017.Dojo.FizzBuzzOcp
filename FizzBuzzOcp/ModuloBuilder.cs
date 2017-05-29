10ed6c47cfc4c21db16356c9152832c146f82b3a
Mathieu Cans <mathieucans@gmail.com>
2017-05-29 18:13:33 +0200

    fizzbang

---
+++ FizzBuzzOcp/ModuloBuilder.cs
@@ -0,0 +1,27 @@
+﻿    using System.Collections.Generic;
+
+namespace FizzBuzzOcp
+{
+    public class ModuloBuilder
+    {
+        private int _modulo;
+        private readonly List<ModuloRule> _moduloRules;
+        private readonly RulesBuilder _rulesBuilder;
+        private string _convertion;
+
+        public ModuloBuilder(RulesBuilder rulesBuilder, List<ModuloRule> moduloRules, int i)
+        {
+            _rulesBuilder = rulesBuilder;
+            _modulo = i;
+            _moduloRules = moduloRules;
+        }
+
+        public RulesBuilder With(string convertion)
+        {
+            _convertion = convertion;
+            _moduloRules.Add(new ModuloRule(_modulo, _convertion));
+            return _rulesBuilder;
+        }
+
+    }
+}
---
+++ FizzBuzzOcp/RulesBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace FizzBuzzOcp
+{
+    public class RulesBuilder
+    {
+        private List<ModuloRule> _moduloRules;
+
+        public RulesBuilder()
+        {
+            _moduloRules = new List<ModuloRule>();
+        }
+        public ModuloBuilder Convert(int i)
+        {
+            var moduloBuilder = new ModuloBuilder(this, _moduloRules, i);            
+            return moduloBuilder;
+        }
+
+        public IEnumerable<IFizzBuzzRule> AndConvertWihtEchoByDefault()
+        {
+            var rules = new List<IFizzBuzzRule>();
+            
+            var concatRule = BuildConcatRules();
+            rules.AddRange(concatRule);
+
+            foreach (var moduloBuilder in _moduloRules)
+            {
+                rules.Add(moduloBuilder);
+            }
+
+            rules.Add(new EchoRule());
+            return rules;
+        }
+
+        private List<ConcatRule> BuildConcatRules()
+        {
+            var reverse = new List<ModuloRule>();
+            reverse.AddRange(_moduloRules);
+            reverse.Reverse();
+
+            var concatRule = new List<ConcatRule>();
+            foreach (var moduloRule in reverse)
+            {
+                foreach (var otherRule in reverse)
+                {
+                    if (otherRule != moduloRule)
+                    {
+                        concatRule.Add(new ConcatRule(new []{moduloRule, otherRule }));
+                    }
+                }
+            }
+            //concatRule.Add(new ConcatRule(reverse));
+            return concatRule;
+        }
+    }
+}
